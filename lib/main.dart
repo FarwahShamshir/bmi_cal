c7b117aa9aa76ecbffd432cdc3cf5be956e0df67
Your Name <your.email@example.com>
2024-07-24 18:24:55 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+
+  var Result = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Center(child: const Text("Your BMI")),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'BMI',
+                style: TextStyle(fontSize: 34),
+              ),
+              TextField(
+                controller: wtController,
+                decoration: InputDecoration(
+                    label: Text('Enter your weight (in kg)'),
+                    prefixIcon: Icon(Icons.line_weight)),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 21.0,
+              ),
+              TextField(
+                controller: ftController,
+                decoration: InputDecoration(
+                    label: Text('Enter your height (in feet)'),
+                    prefixIcon: Icon(Icons.height)),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 21.0,
+              ),
+              TextField(
+                controller: inController,
+                decoration: InputDecoration(
+                    label: Text('Enter your height (in inch)'),
+                    prefixIcon: Icon(Icons.height)),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFC8ACD6), // Correct usage of Color
+                ),
+                onPressed: () {
+                  var wt = wtController.text.toString();
+                  var ft = ftController.text.toString();
+                  var inch = inController.text.toString();
+
+                  if (wt != "" && ft != "" && inch != "") {
+                    var iWt = int.parse(wt);
+                    var iFt = int.parse(ft);
+                    var iInch = int.parse(inch);
+
+                    var tInch = (iFt * 12) + iInch;
+                    var tCm = tInch * 2.54;
+                    var tM = tCm / 100;
+                    var Bmi = iWt / (tM * tM);
+
+                    setState(() {
+                      Result = "Your BMI is: ${Bmi.toStringAsFixed(2)}";
+                    });
+                  } else {
+                    setState(() {
+                      Result = 'Please fill all the required fields';
+                    });
+                  }
+                },
+                child: Text(
+                  'Calculate',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                Result,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
