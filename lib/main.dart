1efbd9e06b2356c6a114cfdc8e695dadd0046315
Farwah Shamshir <farwahshamshir@live.com>
2024-08-29 16:02:37 +0500

    new change

--- lib/main.dart
+++ lib/main.dart
@@ -35,89 +35,106 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Center(child: const Text("Your BMI")),
       ),
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'BMI',
-                style: TextStyle(fontSize: 34),
-              ),
-              TextField(
-                controller: wtController,
-                decoration: InputDecoration(
-                    label: Text('Enter your weight (in kg)'),
-                    prefixIcon: Icon(Icons.line_weight)),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 21.0,
-              ),
-              TextField(
-                controller: ftController,
-                decoration: InputDecoration(
-                    label: Text('Enter your height (in feet)'),
-                    prefixIcon: Icon(Icons.height)),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 21.0,
-              ),
-              TextField(
-                controller: inController,
-                decoration: InputDecoration(
-                    label: Text('Enter your height (in inch)'),
-                    prefixIcon: Icon(Icons.height)),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFC8ACD6), // Correct usage of Color
-                ),
-                onPressed: () {
-                  var wt = wtController.text.toString();
-                  var ft = ftController.text.toString();
-                  var inch = inController.text.toString();
-
-                  if (wt != "" && ft != "" && inch != "") {
-                    var iWt = int.parse(wt);
-                    var iFt = int.parse(ft);
-                    var iInch = int.parse(inch);
-
-                    var tInch = (iFt * 12) + iInch;
-                    var tCm = tInch * 2.54;
-                    var tM = tCm / 100;
-                    var Bmi = iWt / (tM * tM);
-
-                    setState(() {
-                      Result = "Your BMI is: ${Bmi.toStringAsFixed(2)}";
-                    });
-                  } else {
-                    setState(() {
-                      Result = 'Please fill all the required fields';
-                    });
-                  }
-                },
-                child: Text(
-                  'Calculate',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          // Background image with opacity
+          Opacity(
+            opacity: 0.2,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/fff.jpg"), // Replace with your image path
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                Result,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
+            ),
           ),
-        ),
+          // Foreground UI elements
+          Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'BMI',
+                    style: TextStyle(fontSize: 34),
+                  ),
+                  TextField(
+                    controller: wtController,
+                    decoration: InputDecoration(
+                        label: Text('Enter your weight (in kg)'),
+                        prefixIcon: Icon(Icons.line_weight)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21.0,
+                  ),
+                  TextField(
+                    controller: ftController,
+                    decoration: InputDecoration(
+                        label: Text('Enter your height (in feet)'),
+                        prefixIcon: Icon(Icons.height)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21.0,
+                  ),
+                  TextField(
+                    controller: inController,
+                    decoration: InputDecoration(
+                        label: Text('Enter your height (in inch)'),
+                        prefixIcon: Icon(Icons.height)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFC8ACD6), // Correct usage of Color
+                    ),
+                    onPressed: () {
+                      var wt = wtController.text.toString();
+                      var ft = ftController.text.toString();
+                      var inch = inController.text.toString();
+
+                      if (wt != "" && ft != "" && inch != "") {
+                        var iWt = int.parse(wt);
+                        var iFt = int.parse(ft);
+                        var iInch = int.parse(inch);
+
+                        var tInch = (iFt * 12) + iInch;
+                        var tCm = tInch * 2.54;
+                        var tM = tCm / 100;
+                        var Bmi = iWt / (tM * tM);
+
+                        setState(() {
+                          Result = "Your BMI is: ${Bmi.toStringAsFixed(2)}";
+                        });
+                      } else {
+                        setState(() {
+                          Result = 'Please fill all the required fields';
+                        });
+                      }
+                    },
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    Result,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
